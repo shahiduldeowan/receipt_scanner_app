fe7a24e417bd86ca1ef70449b13d4d9e9499cd40
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-15 02:08:45 +0600

    Update build.gradle.kts: set ndkVersion to a specific version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.receipt_scanner_app"
     compileSdk = 35
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
