dc51bb66e00f04d0de32fdba4c4f6e694f97c3b0
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-15 02:09:01 +0600

    Refactor MyApp: integrate BlocProvider for ReceiptCubit and adjust theme settings

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receipt_scanner_app/config/app_config.dart';
 import 'package:receipt_scanner_app/config/app_routes.dart';
 import 'package:receipt_scanner_app/config/app_theme.dart' show ThemeHelper;
+import 'package:receipt_scanner_app/controllers/cubit/receipt_cubit_cubit.dart';
+import 'package:receipt_scanner_app/di/injection.dart';
 import 'package:receipt_scanner_app/utils/app_size_utils.dart';
 
 class MyApp extends StatelessWidget {
@@ -11,21 +14,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return AppSizer(
       builder: (context, orientation, deviceType) {
-        return MaterialApp(
-          title: AppConfig.title,
-          builder: (context, child) {
-            return MediaQuery(
-              data: MediaQuery.of(
-                context,
-              ).copyWith(textScaler: const TextScaler.linear(1.0)),
-              child: child!,
-            );
-          },
-          debugShowCheckedModeBanner: AppConfig.debugShowCheckedModeBanner,
-          themeMode: ThemeMode.light,
-          theme: ThemeHelper.theme,
-          initialRoute: AppRoutes.initialRoute,
-          onGenerateRoute: AppRoutes.onGenerateRoute,
+        return BlocProvider(
+          create: (context) => getIt<ReceiptCubit>(),
+          child: MaterialApp(
+            title: AppConfig.title,
+            builder: (context, child) {
+              return MediaQuery(
+                data: MediaQuery.of(
+                  context,
+                ).copyWith(textScaler: const TextScaler.linear(1.0)),
+                child: child!,
+              );
+            },
+            debugShowCheckedModeBanner: AppConfig.debugShowCheckedModeBanner,
+            themeMode: ThemeMode.dark,
+            darkTheme: ThemeHelper.theme,
+            initialRoute: AppRoutes.initialRoute,
+            onGenerateRoute: AppRoutes.onGenerateRoute,
+          ),
         );
       },
     );
