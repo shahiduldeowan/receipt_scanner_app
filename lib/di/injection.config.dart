8ea9eecbb13638daa64bad171493c2ba3daf9386
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-15 02:09:38 +0600

    Register ReceiptCubit and CameraService in GetIt for dependency injection

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -11,6 +11,9 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:receipt_scanner_app/controllers/cubit/receipt_cubit_cubit.dart'
+    as _i908;
+import 'package:receipt_scanner_app/services/camera_service.dart' as _i304;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -18,7 +21,9 @@ extension GetItInjectableX on _i174.GetIt {
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    _i526.GetItHelper(this, environment, environmentFilter);
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    gh.factory<_i908.ReceiptCubit>(() => _i908.ReceiptCubit());
+    gh.lazySingleton<_i304.CameraService>(() => _i304.CameraService());
     return this;
   }
 }
