6ea555321b3119234fd1bd50767e07e4d9840831
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-15 02:08:54 +0600

    Refactor main.dart: streamline dependency injection in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:receipt_scanner_app/di/injection.dart'
+    show configureDependencies;
 import 'package:receipt_scanner_app/my_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Future.wait([
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]),
-  ]).then((_) {
-    runApp(const MyApp());
-  });
+  ]).then((_) => configureDependencies().then((_) => runApp(const MyApp())));
 }
