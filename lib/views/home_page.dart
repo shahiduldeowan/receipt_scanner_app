e7c76eefdca5fa0286842de58994b33ec8351601
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-15 02:10:07 +0600

    Add camera navigation: implement camera opening functionality in HomePage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:receipt_scanner_app/config/app_routes.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  void _openCamera(BuildContext context) async {
+    Navigator.pushNamed(context, AppRoutes.cameraViewRoute);
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => const CameraView()),
+    // );
+
+    // if (result != null && result is ReceiptModel) {
+    //   context.read<ReceiptBloc>().add(AddReceipt(result));
+    // }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +25,7 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.transparent,
       ),
-      body: Center(
+      body: const Center(
         child: Text(
           'No receipts yet.\nTap the + button to scan.',
           style: TextStyle(fontSize: 16),
@@ -20,7 +33,7 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => _openCamera(context),
         child: const Icon(Icons.add_a_photo),
       ),
     );
