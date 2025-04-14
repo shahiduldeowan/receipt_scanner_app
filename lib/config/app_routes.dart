f9e5fbf6625ad0008ec2a2dc4397704504afdeed
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-15 02:09:09 +0600

    Add camera view route to AppRoutes for navigation

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:receipt_scanner_app/views/camera_view.dart';
 import 'package:receipt_scanner_app/views/home_page.dart';
 
 class AppRoutes {
   static const initialRoute = '/';
+  static const cameraViewRoute = '/camera_view';
 
   static Route? onGenerateRoute(RouteSettings settings) {
     return switch (settings.name) {
       initialRoute => MaterialPageRoute(builder: (_) => const HomePage()),
+      cameraViewRoute => MaterialPageRoute(builder: (_) => const CameraView()),
       _ => null,
     };
   }
